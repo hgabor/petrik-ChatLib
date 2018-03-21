56ae387f5bd613c6fb7572cbabae58991db00c99
Hali <hgabor@level14.hu>
2018-03-21 13:56:41 +0100

    Javadoc

--- src/hu/petrik/chatlib/client/ChatClient.java
+++ src/hu/petrik/chatlib/client/ChatClient.java
@@ -11,27 +11,54 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
-
+/**
+ * Chat kliens osztály, amely segítségével egy chat szerverhez lehet kapcsolódni.
+ */
 public class ChatClient {
-    private String ip;
-    private int port;
+    private final String address;
+    private final int port;
     private Socket socket;
     private OutputStreamWriter writer;
     private BufferedReader reader;
 
-    public ChatClient(String ip, int port) {
-        this.ip = ip;
+    /**
+     * Létrehozza a chat klienst a megadott beállításokkal.
+     * A kapcsolódást nem végzi el, ahhoz a {@link #connect()} függvényt kell meghívni.
+     * 
+     * @param address A szerver web- vagy IP címe
+     * @param port A szerver portja
+     */
+    public ChatClient(String address, int port) {
+        this.address = address;
         this.port = port;
     }
     
+    /**
+     * Kapcsolódik beállított szerverhez egy háttér szálon.
+     * A függvény azonnal visszatér. Kapcsolódási hiba esetén nem itt, hanem
+     * a beállított {@link MessageReceivedListener#error(java.io.IOException) }
+     * függvényében jelzi.
+     * 
+     * @throws IOException 
+     */
     public void connect() throws IOException {
-        socket = new Socket(InetAddress.getByName(ip), port);
+        socket = new Socket(InetAddress.getByName(address), port);
+        
+        // Az írás/olvasás hasonló reader/writer-en keresztül történik, mint pl. a fájlba írás/olvasás.
+        // Mivel a kapcsolat kétirányú, mindkettőből van.
         OutputStream stream = socket.getOutputStream();
         writer = new OutputStreamWriter(stream);
         reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
         startListening();
     }
     
+    /**
+     * Elküldi a megadott üzenetet.
+     * A függvény nem szálbiztos!
+     * 
+     * @param message Az üzenet (vagy parancs)
+     * @throws IOException 
+     */
     public void sendMessage(String message) throws IOException {
         writer.write(message + "\n");
         writer.flush();
@@ -42,23 +69,39 @@ public class ChatClient {
         }
     }
     
+    /**
+     * Elküldi a szervernek a kilépési parancsot, majd bezárja a hálózati kapcsolatot.
+     * A függvény meghívása után minden {@link #sendMessage(java.lang.String)} hívás
+     * meghiúsul. A meghíváskor a hibakezelő eseményben kaphatunk egy {@link IOException} kivételt.
+     * 
+     * @throws IOException 
+     */
     public void close() throws IOException {
         sendMessage("/q");
     }
     
+    /**
+     * Egy új háttérszálon elindítja az üzenetkre való figyelést,
+     * majd továbbítja ezeket az beállított eseménykezelőkhöz.
+     * 
+     * @throws IOException 
+     */
     private void startListening() throws IOException {
         Thread thread = new Thread(new Runnable() {
             @Override
             public void run() {
                 try {
+                    // Végtelen ciklus, mint egy fájlból olvasáskor
                     String line;
                     while ((line = reader.readLine()) != null) {
+                        // A üzenetet továbbítjuk az összes eseménykezelőnek
                         for (MessageReceivedListener listener: receivedListeners) {
                             listener.messageReceived(line);
                         }
                     }
                 }
                 catch (IOException ex) {
+                    // A hibát továbbítjuk az összes eseménykezelőnek
                     for (MessageReceivedListener listener: receivedListeners) {
                         listener.error(ex);
                     }
@@ -68,21 +111,55 @@ public class ChatClient {
         thread.start();
     }
     
+    /**
+     * Ezt az interfészt kell implementálni, ha egy objektum üzeneteket szeretne
+     * fogadni.
+     * Az események a háttérszálon fognak meghívódni, nem az UI szálon!
+     */
     public interface MessageReceivedListener {
+        /**
+         * Kezeli az "Új üzenet" eseményt.
+         * 
+         * @param message Az üzenet
+         */
         public void messageReceived(String message);
+        /**
+         * Kezeli a "Hiba történt" eseményt.
+         * Ha a kliens meghívja ezt a függvényt, feltételezhetjük, hogy a kapcsolat
+         * a továbbiakban használhatatlan.
+         * 
+         * @param ex A hibát jelető kivétel.
+         */
         public void error(IOException ex);
     }
     private List<MessageReceivedListener> receivedListeners = new ArrayList<>();
     
+    /**
+     * Felírja a paramétert az "Új üzenet" és "Hiba történt" eseményekre.
+     * 
+     * @param listener Az eseménykezelő objektum
+     */
     public void addMessageReceivedListener(MessageReceivedListener listener) {
         receivedListeners.add(listener);
     }
+    /**
+     * Törli a paramétert az eseménykezelők közül.
+     * 
+     * @param listener Az eseménykezelő objektum
+     */
     public void removeMessageReceivedListener(MessageReceivedListener listener) {
         receivedListeners.remove(listener);
     }
     
     
-    
+    /**
+     * Teszt metódus, kiírja a konzolra a szervertől kapott üzeneteket.
+     * Ne használjuk együtt az {@link MessageReceivedListener} eseménykezelőkkel!
+     * 
+     * @throws IOException 
+     * @deprecated 
+     */
+    @Deprecated
     private void writeToConsole() throws IOException {
         String line;
         while ((line = reader.readLine()) != null) {
@@ -90,6 +167,15 @@ public class ChatClient {
         }
     }
     
+    /**
+     * Teszt metódus, a konzolról beolvas soronként egy üzenetet, és továbbítja a szervernek.
+     * Ne használjuk együtt a {@link #sendMessage(java.lang.String) } üzenetküldő
+     * függvénnyel!
+     * 
+     * @throws IOException
+     * @deprecated
+     */
+    @Deprecated
     private void readFromConsole() throws IOException {
         Scanner sc = new Scanner(System.in);
         String line;
--- src/hu/petrik/chatlib/server/Client.java
+++ src/hu/petrik/chatlib/server/Client.java
@@ -2,22 +2,37 @@ package hu.petrik.chatlib.server;
 
 import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.net.Socket;
 
+/**
+ * A chat szerveren belül egy kliens kapcsolatot kezel.
+ */
 class Client {
     private Socket socket;
     private ChatServer server;
     private Object syncRoot = new Object();
     private OutputStreamWriter writer;
     
+    /**
+     * Létrehoz egy kliens objektumot a megadott socket-hez.
+     * 
+     * @param socket A kapcsolat a klienshez.
+     * @param server A szerver objektum, amely létrehozta a klienst.
+     */
     public Client(Socket socket, ChatServer server) {
         this.socket = socket;
         this.server = server;
     }
     
+    /**
+     * Fogadja a kliens által küldött üzeneteket, és továbbítja a szervernek.
+     * A függvény blokkoló. Csak egyszer hívjuk meg!
+     * Ha kivételt dob, feltételezhetjük, hogy a kliens kapcsolat használhatatlan.
+     * 
+     * @throws IOException 
+     */
     public void start() throws IOException {
         writer = new OutputStreamWriter(socket.getOutputStream());
         try (BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()))) {
@@ -36,6 +51,13 @@ class Client {
         }
     }
     
+    /**
+     * Elküldi a megadott üzenetet a kliensnek.
+     * A függvény szálbiztos.
+     * 
+     * @param message
+     * @throws IOException 
+     */
     void send(String message) throws IOException {
         synchronized(syncRoot) {
             writer.write(message);
@@ -43,6 +65,12 @@ class Client {
         }
     }
     
+    /**
+     * Leállítja a szervert (a hatására a {@link #start()} függvény kivételt dob).
+     * A függvény szálbiztos.
+     * 
+     * @throws IOException 
+     */
     public void stop() throws IOException {
         socket.close();
     }
